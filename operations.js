6888d687dd9a5d244a98bf33df9e2cb5f1873dd5
Bhargav-Sadineni <sadinenibhargav0@gmail.com>
2026-08-07 19:15:00 +0530

    add operations.js with basic arithmetic and string operations examples

---
+++ operations.js
@@ -0,0 +1,36 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue)
+
+console.log(2-2);
+console.log(2+2);
+console.log(2*3);
+console.log(2**3);
+console.log(2%2);
+
+let str1 = "hello"
+let str2 = "bhargav"
+
+let str3 = str1+str2
+console.log(str3)
+
+console.log("1"+2);
+console.log(1+"2");
+
+// see difference
+// worst code 
+console.log(1+2*3/4) // use brackets it looks too confusion
+console.log("1"+2+2)
+console.log(1+2+"2");
+
+console.log(true)
+console.log(+true) // wrong
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2 // worst readability
+
+let gameCounter = 100
+gameCounter++; // postfix increment here it increments and returns value before incrementing
+++gameCounter;// prefix increments and returns the value after incrementing
+console.log(gameCounter);
--- variables.js
+++ variables.js
@@ -1,8 +1,8 @@
 console.log("Bhargav")
 // variables and constants
-const accountId = 144567
-let accountEmail = "sadinenibhargav0@gmail.com"
-var accountPassword = "12345"
+const accountId = 144567 // here we cannot reaasign also
+let accountEmail = "sadinenibhargav0@gmail.com" // we can only reassign the values
+var accountPassword = "12345" // can redeclare and reassign
 accountCity="guntur"
 let accountState; // if we declare and did not initialize value it is undefined
 
@@ -19,3 +19,9 @@ accountCity="hyd"
 console.log(accountId);
 
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+const user = {name : "bhargav"};
+//const prevents reassignment, not mutation.
+user.name="sadineni";
+//user={} // error 
+console.log(user.name);
